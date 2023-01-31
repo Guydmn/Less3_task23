0dfc4eb02da5efe7bdc5ce776796f6f1ad294a25
Roman Guydmn <romanguydmn@gmail.com>
2023-02-01 02:58:38 +0400

    Задача 23 Семинар 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число которое хотите возвести в куб: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"{Math.Pow(i, 3)}");
+}
